f3037c6227e95796e4bf1580d0ebb0d87459c590
Jordan Harband <ljharb@gmail.com>
2017-12-08 23:21:56 -0800

    [Fix] spec adjustments
     - don’t set length explicitly: https://github.com/tc39/proposal-flatMap/commit/21ac27d54b403777ec147f1dd2254a9e9716396b
     - refactor targetIndex tracking: https://github.com/tc39/proposal-flatMap/commit/a2b06f418571e84448f05f92bffb79724b949bd6
     - FlattenIntoArray: remove “original”: https://github.com/tc39/proposal-flatMap/commit/9da5bfde5ddd2d15118262df5041518a4968339a

--- implementation.js
+++ implementation.js
@@ -5,14 +5,14 @@ var ES = require('es-abstract/es2017');
 var MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || (Math.pow(2, 53) - 1);
 
 // eslint-disable-next-line max-params, max-statements
-var FlattenIntoArray = function FlattenIntoArray(target, original, source, sourceLen, start, depth) {
+var FlattenIntoArray = function FlattenIntoArray(target, source, sourceLen, start, depth) {
 	var targetIndex = start;
 	var sourceIndex = 0;
 
 	/*
 	var mapperFunction;
-	if (arguments.length > 6) {
-		mapperFunction = arguments[6];
+	if (arguments.length > 5) {
+		mapperFunction = arguments[5];
 	}
 	*/
 
@@ -23,24 +23,23 @@ var FlattenIntoArray = function FlattenIntoArray(target, original, source, sourc
 			var element = ES.Get(source, P);
 			/*
 			if (typeof mapperFunction !== 'undefined') {
-				if (arguments.length <= 7) {
+				if (arguments.length <= 6) {
 					throw new TypeError('Assertion failed: thisArg is required when mapperFunction is provided');
 				}
-				element = ES.Call(mapperFunction, arguments[7], [element, sourceIndex, original]);
+				element = ES.Call(mapperFunction, arguments[6], [element, sourceIndex, source]);
 			}
 			*/
-			var spreadable = ES.IsArray(element);
-			if (spreadable && depth > 0) {
+			var flattenable = depth > 0 ? ES.IsArray(element) : false;
+			if (flattenable) {
 				var elementLen = ES.ToLength(ES.Get(element, 'length'));
-				var nextIndex = FlattenIntoArray(target, original, element, elementLen, targetIndex, depth - 1);
-				targetIndex = nextIndex - 1;
+				targetIndex = FlattenIntoArray(target, element, elementLen, targetIndex, depth - 1);
 			} else {
 				if (targetIndex >= MAX_SAFE_INTEGER) {
 					throw new TypeError('index too large');
 				}
 				ES.CreateDataPropertyOrThrow(target, ES.ToString(targetIndex), element);
+				targetIndex += 1;
 			}
-			targetIndex += 1;
 		}
 		sourceIndex += 1;
 	}
@@ -58,7 +57,6 @@ module.exports = function flatten() {
 	}
 
 	var A = ES.ArraySpeciesCreate(O, 0);
-	var nextIndex = FlattenIntoArray(A, O, O, sourceLen, 0, depthNum);
-	ES.Set(A, 'length', nextIndex, true);
+	FlattenIntoArray(A, O, sourceLen, 0, depthNum);
 	return A;
 };
